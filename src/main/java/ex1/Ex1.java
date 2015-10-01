d2e0b4ba734a982df0794d569462a622f12f9663
saeki <Administrator@PC0251>
2015-10-01 14:43:42 +0900

    演習１を実装しました。

--- src/main/java/ex1/Ex1.java
+++ src/main/java/ex1/Ex1.java
@@ -9,6 +9,18 @@ public class Ex1 {
      * 出力例: 55
      */
     public int sum(final int... args) {
-        return 0;
+    	int sum = 0;
+
+    	for(int cnt =0; cnt < args.length; cnt ++){
+    		sum = sum + args[cnt];
+    	}
+
+		return sum;
     }
+
+    //失敗する→グリーンにする→リファクタリングする
+    //このゴールデンループをそのタイミングでやらないと、その後絶対やらないよ！
+
+    //ゴール設定してから、最適なかたちに実装していくんで新人教育にも最適！
+
 }
