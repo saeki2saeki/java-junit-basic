570b31ef839442e54136dce791ce663fc9837044
saeki <Administrator@PC0251>
2015-10-01 14:55:28 +0900

    演習２です。 コードのキレイさとか、気にしてないけどとりあえず動いたのでコミット

--- src/main/java/ex2/Ex2.java
+++ src/main/java/ex2/Ex2.java
@@ -1,17 +1,20 @@
 package ex2;
 
 public class Ex2 {
-    /**
-     * 例題2
-     * 与えられた文字列を反対にしてください。
-     *
-     * 入力例: string
-     * 出力例: gnirts
-     *
-     * @param input 与えられた文字列
-     * @return 反対になった文字列
-     */
-    public String reverse(final String input) {
-        return "";
-    }
+	/**
+	 * 例題2 与えられた文字列を反対にしてください。
+	 *
+	 * 入力例: string 出力例: gnirts
+	 *
+	 * @param input
+	 *            与えられた文字列
+	 * @return 反対になった文字列
+	 */
+	public String reverse(final String input) {
+		String result = "";
+		for (int i = 0; i < input.length(); i++) {
+			result = result + input.substring(input.length() - i - 1, input.length() - i);
+		}
+		return result;
+	}
 }
